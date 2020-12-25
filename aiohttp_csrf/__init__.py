ac0d09d4d7f5e35be32b7ac206e543745a742fcb
TensorTom <14287229+TensorTom@users.noreply.github.com>
2020-12-25 01:11:50 -0500

    Allow up to aiohttp version 4. Bump this version.

--- aiohttp_csrf/__init__.py
+++ aiohttp_csrf/__init__.py
@@ -9,7 +9,7 @@ from .policy import AbstractPolicy
 from .storage import AbstractStorage
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 APP_POLICY_KEY = 'aiohttp_csrf_policy'
 APP_STORAGE_KEY = 'aiohttp_csrf_storage'
